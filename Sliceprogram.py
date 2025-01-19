b34552add25123fba5ce2623be9ccfc4faaede33
Debashish Gogoi <debashishgogoi9577@gmail.com>
2025-01-19 07:43:14 +0530

    Slice all different of operations

---
+++ Sliceprogram.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 3, 4, 5]
+print(my_list[2:4])  #items from one position to another position
+print(my_list[::2])  #items at Specified intervals
+print(my_list[::-1]) #items in reverse order
+print(my_list[-3:])  #items from the end of the list
+print(my_list[:-3])  #items from the beginning of the list
+print(my_list[-3:-1]) #items from the end of the list to the specified position
